d0a74eea48064b34525e02da62cc449de2e32d1b
quimergw <quimergw@localhost>
2018-12-03 11:52:40 +0100

    Fourmi/Foumiliere

--- src/general/Fourmi.java
+++ src/general/Fourmi.java
@@ -1,5 +1,74 @@
 package general;
+import etat.impl.* ;
+import role.impl.*;
 
 public class Fourmi {
+	
+	Boolean FourmiFemelle;
+	Double FourmiPoid;
+	Etat Etat;
+	Reine reine;
+	
+	
+	public Fourmi(Boolean FourmiFemelle, Double FourmiPoid)
+	{
+		FourmiFemelle = this.FourmiFemelle ;
+		FourmiPoid = this.FourmiPoid;
+		this.Etat = new Oeuf();
+		
+	}
+	
+	
+	
+	public Boolean getFourmiFemelle() {
+		return FourmiFemelle;
+	}
+
+
+
+	public void setFourmiFemelle(Boolean fourmiFemelle) {
+		FourmiFemelle = fourmiFemelle;
+	}
+
+
+
+	public Double getFourmiPoid() {
+		return FourmiPoid;
+	}
+
+
+
+	public void setFourmiPoid(Double fourmiPoid) {
+		FourmiPoid = fourmiPoid;
+	}
+
+
+
+	public Etat getEtat() {
+		return Etat;
+	}
+
+
+
+	public void setEtat(Etat etat) {
+		Etat = etat;
+	}
+
+
+
+	public Reine getReine() {
+		return reine;
+	}
+
+
+
+	public void setReine(Reine reine) {
+		this.reine = reine;
+	}
+
+
+	
+	
+	
 
 }
--- src/general/Fourmiliere.java
+++ src/general/Fourmiliere.java
@@ -1,5 +1,40 @@
 package general;
 
+import java.util.*;
+import role.impl.*;
+
 public class Fourmiliere {
+	
+	protected List<Fourmi>  listeFourmis;
+	protected Reine reine;
+	
+	
+	public Fourmiliere(Reine reine)
+	{
+		listeFourmis = new ArrayList<Fourmi>();
+		
+	}
+
+
+	public List<Fourmi> getListeFourmis() {
+		return listeFourmis;
+	}
+
+	public void setListeFourmis(List<Fourmi> listeFourmis) {
+		this.listeFourmis = listeFourmis;
+	}
+
+	public Reine getReine() {
+		return reine;
+	}
+
+	public void setReine() {
+		this.reine = reine;
+	}
+	
+	
+	
+	
 
 }
+
