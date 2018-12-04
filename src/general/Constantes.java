4b911c7014ed7c3cf477213bd1f40ed002435dc8
crennale <crennale@localhost>
2018-12-04 09:05:55 +0100

    costant

--- src/general/Constantes.java
+++ src/general/Constantes.java
@@ -9,6 +9,9 @@ public class Constantes {
 	public final static Color couleurOeuf = new Color(255,255,255);
 	public final static Color couleurLarve = new Color(255,255,0);
 	public final static Color couleurNymphe = new Color(255,153,51);
+	public final static Color couleurReine = new Color(232, 27, 27);
+	public final static Color couleurSoldat = new Color(47, 222, 20);
+	public final static Color couleurOuvriere = new Color(0, 33, 245);
 	public final static int nombreJourMiniReine = 1460;
 	public final static int nombreJourMaxReine = 3650;
 	public final static int nombreJourMiniAdulte = 547;
