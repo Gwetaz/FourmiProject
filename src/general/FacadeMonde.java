8c0df3911e55e4072cf01c99386b9ea5046fff92
rosecnat <rosecnat@localhost>
2018-12-04 08:22:10 +0100

    ihm

--- src/general/FacadeMonde.java
+++ src/general/FacadeMonde.java
@@ -1,13 +1,16 @@
 package general;
 
+import role.impl.Reine;
+
 public class FacadeMonde {
 
 	public final int pourcentOuvriere = 70;
 	public final int pourcentSoldat = 30;
 	
-	public void genererMonde(int nombreOeuf)
+	public void genererMonde(int nombreOeuf) 
 	{
-		Fourmiliere fourmiliere = new Fourmiliere();
+		Reine reine = new Reine();
+		Fourmiliere fourmiliere = new Fourmiliere(reine);
 		int nombreOuvriere = (int) (nombreOeuf*pourcentOuvriere/100);
 		int nombreSoldat = (int) (nombreOeuf*pourcentSoldat/100);
 	}
