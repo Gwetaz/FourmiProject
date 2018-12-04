3c6e085cd8f11e8a8e6855583f453273141fff1e
rosecnat <rosecnat@localhost>
2018-12-04 08:51:29 +0100

    facade

--- src/general/FacadeMonde.java
+++ src/general/FacadeMonde.java
@@ -8,7 +8,7 @@ public class FacadeMonde {
 	
 	public void genererMonde(int nombreOeuf) 
 	{
-		Reine reine = new Reine();
+		Reine reine = new Reine(); 
 		Fourmiliere fourmiliere = new Fourmiliere(reine); 
 		int nombreOuvriere = (int) (nombreOeuf*Constantes.pourcentOuvriere/100);
 		int nombreSoldat = (int) (nombreOeuf*Constantes.pourcentSoldat/100);
