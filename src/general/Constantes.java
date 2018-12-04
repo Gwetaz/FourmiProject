b5da7c59774eaae77af5923136c2fcac132520d9
rosecnat <rosecnat@localhost>
2018-12-04 09:04:22 +0100

    constantes

--- src/general/Constantes.java
+++ src/general/Constantes.java
@@ -1,8 +1,17 @@
 package general;
 
+import java.awt.Color;
+
 public class Constantes {
 
 	public final static int pourcentOuvriere = 70;
 	public final static int pourcentSoldat = 30;
+	public final static Color couleurOeuf = new Color(255,255,255);
+	public final static Color couleurLarve = new Color(255,255,0);
+	public final static Color couleurNymphe = new Color(255,153,51);
+	public final static int nombreJourMiniReine = 1460;
+	public final static int nombreJourMaxReine = 3650;
+	public final static int nombreJourMiniAdulte = 547;
+	public final static int nombreJourMaxAdulte = 912;
 	
 }
