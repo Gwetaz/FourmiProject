4001a39800d5fe3591aa0707267cddfa73345739
rosecnat <rosecnat@localhost>
2018-12-04 11:36:41 +0100

    commit message

--- src/etat/impl/Etat.java
+++ src/etat/impl/Etat.java
@@ -18,6 +18,54 @@ public class Etat {
 		this.positionY = positionY;
 	}
 	
+	public EtatFourmi getEtatFourmi() {
+		return etatFourmi;
+	}
+
+	public void setEtatFourmi(EtatFourmi etatFourmi) {
+		this.etatFourmi = etatFourmi;
+	}
+
+	public int getTempsNaissance() {
+		return tempsNaissance;
+	}
+
+	public void setTempsNaissance(int tempsNaissance) {
+		this.tempsNaissance = tempsNaissance;
+	}
+
+	public double getPoid() {
+		return poid;
+	}
+
+	public void setPoid(double poid) {
+		this.poid = poid;
+	}
+
+	public Color getCouleur() {
+		return couleur;
+	}
+
+	public void setCouleur(Color couleur) {
+		this.couleur = couleur;
+	}
+
+	public int getPositionX() {
+		return positionX;
+	}
+
+	public void setPositionX(int positionX) {
+		this.positionX = positionX;
+	}
+
+	public int getPositionY() {
+		return positionY;
+	}
+
+	public void setPositionY(int positionY) {
+		this.positionY = positionY;
+	}
+
 	protected void changerEtat(EtatFourmi nouvelEtat)
 	{
 		etatFourmi = nouvelEtat;
