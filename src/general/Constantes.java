1ba0c458202f67a6276cb892b7c31f60c9507657
Nathan <Nathan@172.28.127.138>
2018-12-04 16:11:50 +0100

    constantes

---
+++ src/general/Constantes.java
@@ -0,0 +1,30 @@
+package general;
+
+import java.awt.Color;
+
+public class Constantes {
+
+	public final static int pourcentOuvriere = 70;
+	public final static int pourcentSoldat = 30;
+	public final static Color couleurOeuf = new Color(255,255,255);
+	public final static Color couleurLarve = new Color(255,255,0); 
+	public final static Color couleurNymphe = new Color(255,153,51);
+	public final static Color couleurReine = new Color(232, 27, 27);
+	public final static Color couleurSoldat = new Color(47, 222, 20);
+	public final static Color couleurOuvriere = new Color(0, 33, 245);
+	public final static Color couleurFourmiliere = new Color(96, 64, 32);
+	public final static Color couleurSol = new Color(210, 161, 121);
+	public final static int nombreJourMiniReine = 1460;
+	public final static int nombreJourMaxReine = 3650;
+	public final static int nombreJourMiniAdulte = 547;
+	public final static int nombreJourMaxAdulte = 912;
+	public final static int tailleJframeX = 1600;
+	public final static int tailleJframeY = 800;
+	public final static int tailleFourmiliereX = 500;
+	public final static int tailleFourmiliereY = 500;
+	public final static int departFourmiliereX = 550;
+	public final static int departFourmiliereY = 150;
+	public final static int tailleFourmis = 10;
+	public final static int tailleReine = 20;
+	
+}
