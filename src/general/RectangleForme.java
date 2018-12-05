f76900f84fd8af6994a0c17c26291181447c8727
Nathan <Nathan@172.28.127.138>
2018-12-05 13:32:17 +0100

    nid

--- src/general/RectangleForme.java
+++ src/general/RectangleForme.java
@@ -7,10 +7,13 @@ import java.awt.Point;
 
 public class RectangleForme extends Morph{
 	
+	Point p;
+	Dimension dim;
 	
 	public RectangleForme(Color color, Point pos, Dimension dim) {
 		super(color, pos, dim);
-		
+		this.p = pos;
+		this.dim = dim;
 	} 
 
 	public void draw(Graphics g) {
@@ -19,7 +22,7 @@ public class RectangleForme extends Morph{
 		//g.fillOval(bounds.x,bounds.y,bounds.height,bounds.width);
 		//System.out.println("depart x : "+bounds.x+" depart y : "+bounds.y+" taille x : "+bounds.height+" taille y : "+bounds.width);
 
-		g.fillRect(Constantes.departFourmiliereX,Constantes.departFourmiliereY, Constantes.tailleFourmiliereX, Constantes.tailleFourmiliereY);
+		g.fillRect((int) p.getX(),(int) p.getY(), (int) dim.getHeight(), (int) dim.getWidth());
 		g.setColor(c);
 	}
 }
