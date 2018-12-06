0227522ff8364b99673f0052bbc35d8e37282a3b
crennale <crennale@localhost>
2018-12-06 16:42:22 +0100

    ouvriere

--- src/role/impl/Ouvriere.java
+++ src/role/impl/Ouvriere.java
@@ -40,22 +40,81 @@ public class Ouvriere extends Role{
 		}else{
 			f.setPosY(posy-10);
 		}
-		//if(posx)
 	}
 	
-	public static void chasse(Fourmi f) {
-		Random r = new Random();
-		int posx,posy,probabilite;
+	public static void sortirFourmiliere(Fourmi f)
+	{
+		int posx,posy,ecartHaut,ecartBas,ecartDroite,ecartGauche;
+		int finFourX = Constantes.departFourmiliereX+Constantes.tailleFourmiliereX;
+		int finFourY = Constantes.departFourmiliereY+Constantes.tailleFourmiliereY;
 		posx = f.getPosX();
 		posy = f.getPosY();
-		probabilite = r.nextInt(5-1) + 1;
-		if(probabilite == 1)
-			f.setPosX(posx+10);
-		if(probabilite == 2)
-			f.setPosX(posx-10);
-		if(probabilite == 3)
+		ecartHaut = posy - Constantes.departFourmiliereY;
+		ecartBas = finFourY - posy;
+		ecartGauche = posx - Constantes.departFourmiliereX;
+		ecartDroite = finFourX - posx;
+		
+		if(ecartHaut < ecartBas && ecartHaut < ecartGauche && ecartHaut < ecartDroite)
+		{
+			f.setPosY(posy-10);
+		}else if(ecartBas < ecartHaut && ecartBas < ecartGauche && ecartBas < ecartDroite)
+		{
 			f.setPosY(posy+10);
-		if(probabilite == 4)
+		}else if(ecartGauche < ecartBas && ecartGauche < ecartHaut && ecartGauche < ecartDroite)
+		{
+			f.setPosX(posx-10);
+		}else if(ecartDroite < ecartBas && ecartDroite < ecartGauche && ecartDroite < ecartHaut)
+		{
+			f.setPosX(posx+10);
+		}else{
+			f.setPosY(posy-10);
+		}
+	}
+	
+	public static void deplacement(Fourmi f) {
+		Random r = new Random();
+		int prob;
+		prob = r.nextInt(2-1) + 1;
+		if(prob == 1) {
+			deplacementFourmiliere(f);
+		}
+		else if(prob == 2) {
+			deplacementChasse(f);
+		}
+		
+	}
+	
+	public static void deplacementFourmiliere(Fourmi f) {
+		sortirNid(f);
+		Random r = new Random();
+		int posx,posy, prob;
+		posx = f.getPosX();
+		posy = f.getPosY();
+		prob = r.nextInt(5-1) + 1;
+		if(prob == 1)
+			f.setPosX(posx+10);
+		if(prob == 2)
+			f.setPosX(posx-10);
+		if(prob == 3)
+			f.setPosY(posy+10);
+		if(prob == 4)
+			f.setPosY(posy-10);
+	}
+	
+	public static void deplacementChasse(Fourmi f) {
+		sortirFourmiliere(f);
+		Random r = new Random();
+		int posx,posy, prob;
+		posx = f.getPosX();
+		posy = f.getPosY();
+		prob = r.nextInt(5-1) + 1;
+		if(prob == 1)
+			f.setPosX(posx+10);
+		if(prob == 2)
+			f.setPosX(posx-10);
+		if(prob == 3)
+			f.setPosY(posy+10);
+		if(prob == 4)
 			f.setPosY(posy-10);
 	}
 	
@@ -68,6 +127,6 @@ public class Ouvriere extends Role{
 	}
 	
 	public static void actionOuvriere(Fourmi f) {
-		chasse(f);
+		deplacement(f);
 	}
 }
