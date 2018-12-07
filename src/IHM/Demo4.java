6f298ff6882cdf7cdca0b0ffa2f3547db67e0ce7
crennale <crennale@localhost>
2018-12-07 13:04:26 +0100

    kk

--- src/IHM/Demo4.java
+++ src/IHM/Demo4.java
@@ -38,6 +38,7 @@ public class Demo4 {
 		listeStack.add(s2);
 		listeStack.add(s3);
 		listeStack.add(s4);
+		fourmiliere.setListeStack(listeStack);
 		RectangleForme nidForme = FacadeMonde.genererNid(nid);
 		List<Cercle> listeCercle = FacadeMonde.genererListCercle(fourmiliere);
 		List<Cercle> listeProies = new ArrayList<Cercle> ();
